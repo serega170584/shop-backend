129474e16c72887db3e3ff8fe9ea79c765b3515c
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2021-01-21 14:09:18 +0300

    Paginator refactoring

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PageEvent } from '@angular/material/paginator';
-import { FormControl, NgForm } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { FormControl } from '@angular/forms';
+import { ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface PeriodicElement {
   name: string;
@@ -16,11 +18,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 6, name: 'Barbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Bitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Bxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Bluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Beon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
@@ -29,35 +31,36 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource: PeriodicElement[] = ELEMENT_DATA;
-  length = 0;
-  pageSize = 0;
+  filteredDataSource: PeriodicElement[] = ELEMENT_DATA;
   pageProducts: Object[] = [];
   pageSizeOptions: number[] = [];
-  pageIndex: number = 0;
   formControl!: FormControl;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.formControl=new FormControl();
+    this.formControl = new FormControl();
     this.formControl.setValue('');
-    this.length = this.dataSource.length;
-    let length = this.length;
-    this.pageSizeOptions = this.calculatePageSizeOptions(length);
-    if (length > 2 && this.pageSizeOptions.length == 2) {
-      this.pageSizeOptions = this.calculatePageSizeOptions(length + 1);
-    } else {
-      this.pageSizeOptions.pop();
-      this.pageSizeOptions = this.pageSizeOptions.slice(1);
-    }
-    this.pageSize = this.pageSizeOptions[0];
-    this.pageProducts = this.getFilteredDataSource().slice(this.pageIndex, this.pageSize);
+    this.formControl.valueChanges.subscribe(v => this.onChange());
+    this.pageSizeOptions = this.getPageSizeOptions();
   }
 
-  calculatePageSizeOptions(length: number): number[] {
+  onChange(): void {
+    this.pageSizeOptions = this.getPageSizeOptions();
+    this.paginator.firstPage();
+  }
+
+  getCalculatedPageSizeOptions(length: number): number[] {
     let pageSize: number = 0;
+    this.pageSizeOptions = [];
     for (let i = 0; i < length; ++i) {
       pageSize = i + 1;
       if ((length % pageSize) == 0) {
@@ -67,22 +70,24 @@ export class ProductsComponent implements OnInit {
     return this.pageSizeOptions;
   }
 
-  onPage($event: PageEvent): void {
-    let fromIndex: number = $event.pageIndex * $event.pageSize;
-    let toIndex: number = ($event.pageIndex + 1) * $event.pageSize;
-    this.pageProducts = this.getFilteredDataSource().slice(fromIndex, toIndex);
+  getPageSizeOptions(): number[] {
+    let length = this.dataSource.data.length;
+    this.pageSizeOptions = this.getCalculatedPageSizeOptions(length);
+    if (length > 2 && this.pageSizeOptions.length == 2) {
+      this.pageSizeOptions = [];
+      this.pageSizeOptions = this.getCalculatedPageSizeOptions(length + 1);
+    }
+    this.pageSizeOptions.pop();
+    this.pageSizeOptions = this.pageSizeOptions.slice(1);
+    return this.pageSizeOptions;
   }
 
   onSubmit($event: Event): void {
     $event.preventDefault();
-    this.formControl.setValue('888888');
-  }
-
-  getFilteredDataSource(): PeriodicElement[] {
-    return this.dataSource.filter(item => this.isProductSearched(item));
-  }
-
-  isProductSearched(item: PeriodicElement): boolean {
-    return item.name.startsWith(this.formControl.value);
+    this.dataSource.filter = 'Вo';
+    this.dataSource.filterPredicate = (data: PeriodicElement, filter: string) => {
+      // console.log(data);
+      return true;
+    };
   }
 }
